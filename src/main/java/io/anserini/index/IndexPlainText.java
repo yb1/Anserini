1b21250f7158d69ed295b233a71991473e8bba3a
Youngbin Kim <ykim828@hotmail.com>
2016-06-05 13:38:57 -0400

    indexer writes to hdfs

--- src/main/java/io/anserini/index/IndexPlainText.java
+++ src/main/java/io/anserini/index/IndexPlainText.java
@@ -2,6 +2,7 @@ package io.anserini.index;
 
 import org.apache.commons.cli.*;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -16,6 +17,8 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.RAMDirectory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -35,50 +38,55 @@ public class IndexPlainText {
     public static final String FIELD_BODY = "body";
     public static final String FIELD_URL = "url";
 
-    public static void writeIndex(FileSystem fs, IndexWriter writer, FileStatus[] status) {
+    public static void writeIndex(FileSystem fs, Path pt, IndexWriter writer) {
+        String url;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(pt)));
+
+            String row;
+
+            while ((row=reader.readLine())!= null)
+            {
+                String tokens[] = row.split("\\s", 2);
+
+                if (tokens.length < 2) {
+                    LOG.error("Cannot tokenize for url {} from path {} ", tokens[0], pt.getName());
+                    continue;
+                }
+
+                url = tokens[0];
+                String body = tokens[1];
+
+                if (url == null || body == null) {
+                    continue;
+                }
+
+                Document document = new Document();
+
+                document.add(new StringField(FIELD_URL, url, Field.Store.YES));
+                document.add(new TextField(FIELD_BODY, body, Field.Store.NO));
+                writer.addDocument(document);
+            }
+            reader.close();
+        } catch (Exception e) {
+            LOG.error("CANNOT write index for url {} from path {} ", pt.getName(), e);
+        }
+    }
+
+    public static void iterateFiles(FileSystem fs, IndexWriter writer, FileStatus[] status) {
         for (int i = 0; i < status.length; i++) {
             FileStatus file = status[i];
             Path pt = file.getPath();
-            String url;
+
             if (file.isDirectory()) {
                 try {
-                    writeIndex(fs, writer, fs.listStatus(pt));
+                    iterateFiles(fs, writer, fs.listStatus(pt));
                 } catch (Exception e) {
                     LOG.error("CANNOT access subdirectory for a directory {} ", pt.getName(), e);
                 }
 
             } else {
-                try {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(pt)));
-
-                    String row;
-
-                    while ((row=reader.readLine())!= null)
-                    {
-                        String tokens[] = row.split("\\s", 2);
-
-                        if (tokens.length < 2) {
-                            LOG.error("Cannot tokenize for url {} ", tokens[0]);
-                            continue;
-                        }
-
-                        url = tokens[0];
-                        String body = tokens[1];
-
-                        if (url == null || body == null) {
-                            continue;
-                        }
-
-                        Document document = new Document();
-
-                        document.add(new StringField(FIELD_URL, url, Field.Store.YES));
-                        document.add(new TextField(FIELD_BODY, body, Field.Store.NO));
-                        writer.addDocument(document);
-                    }
-                    reader.close();
-                } catch (Exception e) {
-                    LOG.error("CANNOT write index for url {} from path {} ", pt.getName(), e);
-                }
+                writeIndex(fs, pt, writer);
             }
         }
     }
@@ -122,17 +130,43 @@ public class IndexPlainText {
         conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
 
 
-        Directory dir = FSDirectory.open(Paths.get(indexPath));
+        RAMDirectory rdir = new RAMDirectory();
+        //Directory dir = FSDirectory.open(Paths.get(indexPath));
         final IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
 
         config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 
-        IndexWriter writer = new IndexWriter(dir, config);
+        IndexWriter writer = new IndexWriter(rdir, config);
         FileSystem fs = FileSystem.get(conf);
         FileStatus[] status = fs.listStatus(new Path(inputPath));
-        writeIndex(fs, writer, status);
+        iterateFiles(fs, writer, status);
 
         writer.close();
 
+        // Getting files present in memory into an array.
+        String fileList[]=rdir.listAll();
+
+// Reading index files from memory and storing them to HDFS.
+        for (int i = 0; i < fileList.length; i++)
+        {
+            IndexInput indxfile = rdir.openInput(fileList[i].trim(), null);
+            long len = indxfile.length();
+            int len1 = (int) len;
+
+            // Reading data from file into a byte array.
+            byte[] bytarr = new byte[len1];
+            indxfile.readBytes(bytarr, 0, len1);
+
+// Creating file in HDFS directory with name same as that of
+//index file
+            Path src = new Path(fs.getWorkingDirectory() + "/" + indexPath + "/" + fileList[i].trim());
+            fs.createNewFile(src);
+
+            // Writing data from byte array to the file in HDFS
+            FSDataOutputStream ofs = fs.create(new Path(fs.getWorkingDirectory() + "/" + indexPath + "/" + fileList[i].trim()),true);
+            ofs.write(bytarr);
+            ofs.close();
+        }
+        fs.closeAll();
     }
 }
--- src/main/java/io/anserini/search/SearchPlainText.java
+++ src/main/java/io/anserini/search/SearchPlainText.java
@@ -1,6 +1,11 @@
 package io.anserini.search;
 
 import io.anserini.index.IndexPlainText;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
@@ -11,9 +16,7 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.MMapDirectory;
+import org.apache.lucene.store.*;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.OptionHandlerFilter;
@@ -52,7 +55,43 @@ public class SearchPlainText {
             dir = FSDirectory.open(Paths.get(searchArgs.index));
         }
 
-        DirectoryReader ireader = DirectoryReader.open(dir);
+        Configuration conf = new Configuration();
+        conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
+
+        FileSystem dfs = FileSystem.get(conf);
+
+// Creating a RAMDirectory (memory) object, to be able to create index in memory.
+        RAMDirectory rdir = new RAMDirectory();
+
+        String indexPath = searchArgs.index;
+
+        FileStatus[] status = dfs.listStatus(new Path(indexPath));
+
+        FSDataInputStream filereader = null;
+        for (int i = 0; i < status.length; i++)
+        {
+// Reading data from index files on HDFS directory into filereader object.
+            String fileName = status[i].getPath().getName();
+            filereader = dfs.open(new Path(dfs.getWorkingDirectory() + "/" + indexPath + "/" + fileName));
+
+            int size = filereader.available();
+
+            // Reading data from file into a byte array.
+            byte[] bytarr = new byte[size];
+            filereader.read(bytarr, 0, size);
+
+// Creating file in RAM directory with names same as that of
+//index files present in HDFS directory.
+            IndexOutput indxout = rdir.createOutput(fileName, null);
+
+            // Writing data from byte array to the file in RAM directory
+            indxout.writeBytes(bytarr,bytarr.length);
+            indxout.close();
+        }
+        filereader.close();
+
+        DirectoryReader ireader = DirectoryReader.open(rdir);
         IndexSearcher isearcher = new IndexSearcher(ireader);
 
         Analyzer analyzer = new StandardAnalyzer();
@@ -68,6 +107,7 @@ public class SearchPlainText {
             Document hitDoc = isearcher.doc(hits[i].doc);
             System.out.println(hitDoc.get("url"));
         }
+        rdir.close();
         ireader.close();
         dir.close();
     }
